860e175d0324e1071827968db4fb18257359b871
Erik Wartenberg <erik@studiobosco.de>
2023-01-19 09:10:04 +0100

    built basic workflow for downloading page source and taking screenshot

--- downloadBot.js
+++ downloadBot.js
@@ -3,7 +3,11 @@ const path = require('path')
 const https = require('https')
 const HTMLParser = require('node-html-parser');
 const emoji = require('node-emoji')
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, promise, until} = require('selenium-webdriver');
+const { promisify } = require('util');
+const firefox = require('selenium-webdriver/firefox');
+
+require('geckodriver');
 
 const baseUrl = ''
 
@@ -11,25 +15,63 @@ const baseUrl = ''
 const targetFileTypes = ['image', 'pdf', 'spreadsheet']
 
 
-const webDriverTest = async () => {
+let options = new firefox.Options()
+    // .headless()
 
-    let driver = await new Builder().forBrowser('firefox').build();
+let driver = new Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(options)
+    .build();
 
+console.log('\n');
+console.log(emoji.get("arrow_down"), ' ',`Startet crawling process for ${baseUrl}...`);
+console.log('\n');
+
+async function main() {
     try {
-        await driver.get(baseUrl)
-        await driver.findElement(By.css('')).click()
-        await driver.wait(until.titleIs(''), 1000)
-        await driver.findElement(By.css("[name='username']")).sendKeys('')
-        await driver.findElement(By.css("[name='password']")).sendKeys('')
-        await driver.findElement(By.css("")).click()
-        await driver.wait(until.titleIs(''), 1000)
-
+        await driver.get(baseUrl);
+        console.log(emoji.get("key"), ' ',`Entered ${baseUrl}...`);
+        console.log('\n');
+        const source = await driver.getPageSource();
+        const screenshot = await driver.takeScreenshot();
+        console.log(emoji.get("female-detective"), ' ',`Analyzing page source code for ${baseUrl}...`);
+        console.log('\n');
+        fs.writeFileSync('test.html', source, err => console.log(err))
+        fs.writeFileSync('screenshot.png', screenshot, 'base64', err => console.log(err))
     } finally {
-        await driver.quit()
+        await driver.quit();
+        console.log(emoji.get("white_check_mark"), ' ',`Downloaded data!`);
+        console.log('\n');
+        console.log(emoji.get("partying_face"), ' ', "\x1b[32m", 'Woohoo, all downloads completed!', ' ', emoji.get("champagne"));
+        console.log('\n');
     }
-
 }
 
+    
+
+main();
 
 
-webDriverTest()
+
+// const webDriverTest = async () => {
+
+//     let driver = await new Builder().forBrowser('firefox').build();
+
+//     try {
+//         await driver.get(baseUrl)
+//         await driver.findElement(By.css('')).click()
+//         await driver.wait(until.titleIs(''), 1000)
+//         await driver.findElement(By.css("[name='username']")).sendKeys('')
+//         await driver.findElement(By.css("[name='password']")).sendKeys('')
+//         await driver.findElement(By.css("")).click()
+//         await driver.wait(until.titleIs(''), 1000)
+
+//     } finally {
+//         await driver.quit()
+//     }
+
+// }
+
+
+
+// webDriverTest()
