8d7fd089b65045b4f59129a83a9782e27486831c
Erik Wartenberg <erik@studiobosco.de>
2023-01-21 20:57:52 +0100

    added browser config, built async downlaod loop

--- downloadBot.js
+++ downloadBot.js
@@ -1,77 +1,97 @@
 const fs = require('fs')
 const path = require('path')
-const https = require('https')
-const HTMLParser = require('node-html-parser');
 const emoji = require('node-emoji')
-const {Builder, By, Key, promise, until} = require('selenium-webdriver');
-const { promisify } = require('util');
-const firefox = require('selenium-webdriver/firefox');
-
+const {Builder, By, Key, promise, until} = require('selenium-webdriver')
+const firefox = require('selenium-webdriver/firefox')
 require('geckodriver');
 
-const baseUrl = ''
+const loginUrl = ''
+const invoicesUrl = ''
+const userKey = ''
+const passwordKey = ''
 
 // add file types to observe/download here
 const targetFileTypes = ['image', 'pdf', 'spreadsheet']
 
 
 let options = new firefox.Options()
-    // .headless()
+    .headless()
+    .setPreference("browser.link.open_newwindow", 1)
+    .setPreference("pdfjs.enabledCache.state", false)
+    .setPreference("browser.download.open_pdf_attachments_inline", true)
+    .setPreference("browser.download.start_downloads_in_tmp_dir", true)
+    .setPreference("browser.download.folderList", 2)
+    .setPreference("browser.download.manager.showWhenStarting", false)
+    .setPreference("browser.download.dir", path.join(__dirname, 'assets/downloads'))
+    .setPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+    .setBinary("/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox-bin")
+    .setProfile("")
 
 let driver = new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(options)
-    .build();
+    .build()
 
 console.log('\n');
-console.log(emoji.get("arrow_down"), ' ',`Startet crawling process for ${baseUrl}...`);
+console.log(emoji.get("arrow_down"), ' ',`Started crawling process for ${loginUrl}...`);
 console.log('\n');
 
-async function main() {
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+const fileAlreadyExists = (file,directory) => {
+    return fs.existsSync(path.join(__dirname, `${directory}${file}`))
+}
+
+const downloadFiles = async fileUrls => {
+    for (const fileUrl of fileUrls) {
+        const href = await fileUrl.getAttribute('href')
+        const urlSegments = href.split('/')
+        const fileName = urlSegments[urlSegments.length - 1]
+        
+        if (fileAlreadyExists(fileName,'assets/downloads/Webflow-')) {
+            console.log(emoji.get("repeat"), ' ',`File ${fileName} already downloaded...`);
+            continue
+        }
+
+        fileUrl.click()
+
+        while (!fileAlreadyExists(fileName,'assets/downloads/Webflow-')) {
+            await sleep(1)
+        }
+
+        console.log(emoji.get("white_check_mark"), ' ',`Downloaded ${fileName}!`);
+    }
+
+    return
+}
+
+const main = async () => {
     try {
-        await driver.get(baseUrl);
-        console.log(emoji.get("key"), ' ',`Entered ${baseUrl}...`);
+        await driver.get(loginUrl);
+        console.log(emoji.get("desktop_computer"), ' ',`Entered ${loginUrl}...`);
         console.log('\n');
-        const source = await driver.getPageSource();
-        const screenshot = await driver.takeScreenshot();
-        console.log(emoji.get("female-detective"), ' ',`Analyzing page source code for ${baseUrl}...`);
+
+        const userFormField = await driver.findElement(By.css("[name='username']")).sendKeys(userKey)
+        const passwordFormField = await driver.findElement(By.css("[name='password']")).sendKeys(passwordKey)
+        await driver.findElement(By.css("[data-automation-id='login-button']")).click()
+        await driver.wait(until.titleIs(''))
+        console.log(emoji.get("key"), ' ',`Login successfully for ${loginUrl}...`);
         console.log('\n');
-        fs.writeFileSync('test.html', source, err => console.log(err))
-        fs.writeFileSync('screenshot.png', screenshot, 'base64', err => console.log(err))
-    } finally {
-        await driver.quit();
-        console.log(emoji.get("white_check_mark"), ' ',`Downloaded data!`);
+
+        await driver.get(invoicesUrl)
+        const fileUrls = await driver.wait(until.elementsLocated(By.css('#invoices a[ng-href]')))
+        
+        await downloadFiles(fileUrls)
+
+        await sleep(1000)
+    } 
+
+    finally {
         console.log('\n');
         console.log(emoji.get("partying_face"), ' ', "\x1b[32m", 'Woohoo, all downloads completed!', ' ', emoji.get("champagne"));
         console.log('\n');
+        await driver.quit();
     }
 }
 
-    
-
-main();
-
-
-
-// const webDriverTest = async () => {
-
-//     let driver = await new Builder().forBrowser('firefox').build();
-
-//     try {
-//         await driver.get(baseUrl)
-//         await driver.findElement(By.css('')).click()
-//         await driver.wait(until.titleIs(''), 1000)
-//         await driver.findElement(By.css("[name='username']")).sendKeys('')
-//         await driver.findElement(By.css("[name='password']")).sendKeys('')
-//         await driver.findElement(By.css("")).click()
-//         await driver.wait(until.titleIs(''), 1000)
-
-//     } finally {
-//         await driver.quit()
-//     }
-
-// }
-
-
-
-// webDriverTest()
+main()
